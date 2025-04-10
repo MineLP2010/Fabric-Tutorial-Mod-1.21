920cd4dfa9af6f105a0a26cf58cce2977f628d6f
JefreyTheSquirrl <MineLP69@gmail.com>
2025-04-10 19:03:32 +0200

    added Pink Garnet and Raw Pink Garnet

--- src/main/java/net/jefreythesquirrl/tutorialmod/TutorialMod.java
+++ src/main/java/net/jefreythesquirrl/tutorialmod/TutorialMod.java
@@ -2,16 +2,16 @@ package net.jefreythesquirrl.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.jefreythesquirrl.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
