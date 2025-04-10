d765cd4f1a05604bfcf6d6989504e30933361fb3
JefreyTheSquirrl <MineLP69@gmail.com>
2025-04-11 00:47:03 +0200

    added two creative mode tabs for the new items

--- src/main/java/net/jefreythesquirrl/tutorialmod/TutorialMod.java
+++ src/main/java/net/jefreythesquirrl/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.jefreythesquirrl.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.jefreythesquirrl.tutorialmod.block.ModBlocks;
+import net.jefreythesquirrl.tutorialmod.item.ModItemGroups;
 import net.jefreythesquirrl.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
